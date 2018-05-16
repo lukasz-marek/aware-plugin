55b37caa02da93641d4befd3332f93d3559a50a4
Lukasz Marek <marek.lucas@gmail.com>
2018-05-16 17:27:12 +0200

    added option to control active sensors

--- com.aware.plugin.template/src/main/java/com/aware/plugin/template/Settings.java
+++ com.aware.plugin.template/src/main/java/com/aware/plugin/template/Settings.java
@@ -1,11 +1,15 @@
 package com.aware.plugin.template;
 
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
 
 import com.aware.Aware;
 import com.aware.plugin.template.communication.MessageRecipient;
@@ -19,7 +23,7 @@ public class Settings extends PreferenceActivity implements SharedPreferences.On
     public static final String STATUS_PLUGIN_TEMPLATE = "status_plugin_template";
 
     //Plugin settings UI elements
-    private static CheckBoxPreference status;
+    private static volatile CheckBoxPreference status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +43,31 @@ public class Settings extends PreferenceActivity implements SharedPreferences.On
     @Override
     protected void onResume() {
         super.onResume();
+        isStoragePermissionGranted();
 
-        status = (CheckBoxPreference) findPreference(STATUS_PLUGIN_TEMPLATE);
-        if( Aware.getSetting(this, STATUS_PLUGIN_TEMPLATE).length() == 0 ) {
-            Aware.setSetting( this, STATUS_PLUGIN_TEMPLATE, true ); //by default, the setting is true on install
+    }
+
+    private boolean isStoragePermissionGranted() {
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            return false;
         }
-        status.setChecked(Aware.getSetting(getApplicationContext(), STATUS_PLUGIN_TEMPLATE).equals("true"));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            status = (CheckBoxPreference) findPreference(STATUS_PLUGIN_TEMPLATE);
+            if( Aware.getSetting(this, STATUS_PLUGIN_TEMPLATE).length() == 0 ) {
+                Aware.setSetting( this, STATUS_PLUGIN_TEMPLATE, true ); //by default, the setting is true on install
+            }
+            status.setChecked(Aware.getSetting(getApplicationContext(), STATUS_PLUGIN_TEMPLATE).equals("true"));
+        }
     }
 
     @Override
